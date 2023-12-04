95b2602396ea579606d4fa26cede4538e50995a7
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-04 10:53:03 +0300

    Initial Commit

---
+++ MixedTextApp/MixedTextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixedTextAppApp.swift
+//  MixedTextApp
+//
+//  Created by jopootrivatel on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MixedTextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
